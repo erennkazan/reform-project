7818a71adfa5842a10b016229436d10861a21a1f
erennkazan@gmail.com <erennkazan@gmail.com>
2023-06-17 19:54:42 +0300

    project created

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}" ,"./node_modules/flowbite/**/*.js"],
+    theme: {
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+      extend: {
+      },
+    },
+    plugins: [
+      require('flowbite/plugin')
+    ],
+     
+  }
+  
