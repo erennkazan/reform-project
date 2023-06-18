8b4e6741be823f0d2526d7c7c96894ef37bfd4d8
erennkazan@gmail.com <erennkazan@gmail.com>
2023-06-18 14:11:24 +0300

    header component added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}" ,"./node_modules/flowbite/**/*.js"],
-    theme: {
-      screens: {
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
-      extend: {
-        fontFamily: {
-          'font-family': ['Montserrat', 'sans-serif'],
-        },
+  content: ["./src/**/*.{html,js}" ,"./node_modules/flowbite/**/*.js"],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    spacing: {
+      '0': '0',
+      '36': '9rem',
+    },
+    extend: {
+      fontFamily: {
+        'font-family': ['Montserrat', 'sans-serif'],
       },
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
-     
-  }
-  
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+};
